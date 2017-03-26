98490df3c100af9531073482e6ca3a9987f2219b
Anthony Ratliff <arratlif@uncg.edu>
2017-03-26 16:31:54 -0400

    Added the DES Encryption class to the Key Manager of DH.  Works great.

--- DH/src/KeyManager.java
+++ DH/src/KeyManager.java
@@ -12,12 +12,19 @@ import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyAgreement;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.interfaces.DHPrivateKey;
 import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.DHParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class KeyManager{
     protected KeyPairGenerator kpg;
@@ -95,4 +102,82 @@ class DHKeyManager extends KeyManager {
         }
         return super.keyAgree.generateSecret();
     }
+}
+
+class DESEncryption {
+    // private class fields
+    private final String ENCRYPTION_ALGO = "DES/ECB/PKCS5Padding";
+    private final int KEYSIZE = 8;
+    private SecretKeySpec secretKey;
+    private Cipher cipher;
+    private byte[] key;
+    
+    
+    // class constructor
+    public DESEncryption(byte[] sharedkey) {
+        if (sharedkey.length == KEYSIZE){
+            this.key = sharedkey;
+        } else {
+            this.key = new byte[KEYSIZE];
+        }
+        this.initCipher();
+    }
+    
+    public byte[] getKey(){
+        return this.key;
+    }
+    
+    public boolean setKey(byte[] input){
+        if (input.length == KEYSIZE) {
+            this.key = input;
+            return true;
+        }
+        return false;
+    }
+    
+    private void initCipher(){
+        try {
+            this.secretKey = new SecretKeySpec(this.key, 0, this.key.length, "ENCRYPTION_ALGO");
+            cipher = Cipher.getInstance(ENCRYPTION_ALGO);
+            System.out.println("Keysize: " + this.key.length);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException ex) {
+            Logger.getLogger(DESEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String encryptDES(String plaintext){
+        String ciphertext = "";
+        try {
+            SecretKeyFactory sf = SecretKeyFactory.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, sf.generateSecret(secretKey));
+            byte[] plainBytes = plaintext.getBytes();
+            byte[] cipherBytes = cipher.doFinal(plainBytes);
+            ciphertext = new String(Base64.getEncoder().encode(cipherBytes));
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(DESEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(DESEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeySpecException ex) {
+            Logger.getLogger(DESEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ciphertext;
+    }
+    
+    public String decryptDES(String ciphertext){
+        String plaintext = "";
+        try { 
+            SecretKeyFactory sf = SecretKeyFactory.getInstance("DES");
+            byte[] cipherBytes = Base64.getDecoder().decode(ciphertext);  // Encrypted string as Base64 decoder.
+            cipher.init(Cipher.DECRYPT_MODE, sf.generateSecret(secretKey));
+            plaintext = new String(cipher.doFinal(cipherBytes));  // Decrypt the original message.
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(DESEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(DESEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeySpecException ex) {
+            Logger.getLogger(DESEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return plaintext;
+    }
+    
 }
