59b39a533124ace955e494605c0ed16cbe49dfd7
Anthony Ratliff <arratlif@uncg.edu>
2017-03-26 13:45:39 -0400

    The DH section now sends and receives using Base64 encoding/decoding.  I also cleaned up the code and commented a few sections.

--- DH/src/client.java
+++ DH/src/client.java
@@ -6,23 +6,29 @@ import java.util.Base64;
 class TCPClient {
 
     public static void main(String argv[]) throws Exception {
+        // Declare a key manager and set the Diffie-Hellman parameters.
         DHKeyManager keyman1 = new DHKeyManager(1024);
         keyman1.generateParameters();
+        
+        // method fields used to hold strings to be read and written.
         String sentence;
         String modifiedSentence;
+        
+        // Readers and Writers used to communicate across TCP/IP connection.
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         Socket clientSocket = new Socket("localhost", 6789);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         DataInputStream inFromServer = new DataInputStream(clientSocket.getInputStream());
-        //BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+        // Key Agreement
         byte[] message = keyman1.returnMyPublicKey();
-        outToServer.writeInt(message.length);
-        outToServer.write(message);
-        int length = inFromServer.readInt();
-        byte[] theirKey = new byte[length];
-        inFromServer.readFully(theirKey, 0, theirKey.length);
+        outToServer.writeUTF(new String(Base64.getEncoder().encode(message)));
+        String key = inFromServer.readUTF();
+        byte[] theirKey = Base64.getDecoder().decode(key);
         keyman1.setTheirPublicKey(theirKey);
         byte[] outputA = keyman1.computeSharedSecret();
+        
+        // Displays the output of the Key Agreement.
         String encoded = new String(Base64.getEncoder().encode(outputA));
         System.out.println("Computed Shared Secret: ");
         System.out.println(encoded);
--- DH/src/server.java
+++ DH/src/server.java
@@ -6,28 +6,34 @@ import java.util.Base64;
 class TCPServer {
 
     public static void main(String argv[]) throws Exception {
+        // Declare a key manager and set the Diffie-Hellman parameters.
         DHKeyManager keyman2 = new DHKeyManager(1024);
+        
+        // method fields used to hold strings to be read and written.
         String clientSentence;
         String capitalizedSentence;
+        
+        // Declare and initialize the server socket to listen on port 6789.
         ServerSocket welcomeSocket = new ServerSocket(6789);
         System.out.println("Listening for connection on port 6789");
 
         while (true) {
             Socket connectionSocket = welcomeSocket.accept();
             System.out.println("Connection received.");
-            //BufferedReader inFromClient
-                    //= new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            
+            // Once a connection has been established, create the readers and writers used to communicate across TCP/IP connection.
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             DataInputStream inFromClient = new DataInputStream(connectionSocket.getInputStream());
-            int length = inFromClient.readInt();
-            byte[] theirKey = new byte[length];
-            inFromClient.readFully(theirKey, 0, theirKey.length);
-            //clientSentence = inFromClient.readLine();
+            
+            // Perform Key Agreement.
+            String key = inFromClient.readUTF();
+            byte[] theirKey = Base64.getDecoder().decode(key);
             keyman2.setTheirPublicKey(theirKey);
             keyman2.setDHParameterSpec();
             byte[] myKey = keyman2.returnMyPublicKey();
-            outToClient.writeInt(myKey.length);
-            outToClient.write(myKey);
+            outToClient.writeUTF(new String(Base64.getEncoder().encode(myKey)));
+
+            // Display shared secrete.
             byte[] outputB = keyman2.computeSharedSecret();
             String encoded = new String(Base64.getEncoder().encode(outputB));
             System.out.println("Computed Shared Secret: ");
